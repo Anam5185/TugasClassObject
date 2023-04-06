cd0031b4e7f11a1e9377efb275deac1977b802ea
Anam5185 <128022458+Anam5185@users.noreply.github.com>
2023-04-06 14:54:22 +0700

    Add files via upload

---
+++ TugasTaxi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    class TugasTaxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            string duty = "";
+            if (OnDuty == true)
+            {
+                duty = "Yes";
+            }
+            else if (OnDuty == false)
+            {
+                duty = "No";
+            }
+
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", duty);
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang\n", DriverName);
+        }
+    }
+}
